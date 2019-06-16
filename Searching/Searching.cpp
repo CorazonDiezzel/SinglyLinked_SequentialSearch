0d39f5cf63d1a3f3f3167e17a1a4c3880678f411
Corazon Diezzel <dharmawan.takdir@gmail.com>
2019-06-16 15:50:33 +0800

    Add project files.

---
+++ Searching/Searching.cpp
@@ -0,0 +1,159 @@
+#include <iostream>
+#include <conio.h>
+#include <vector>
+
+//Struct
+typedef struct TNode {
+	int data;
+	TNode *next;
+};
+
+//Pointers;
+TNode* baru, * awal, * akhir, * bantu_tampil, *bantu_cari;
+
+//Variables;
+
+
+//Functions
+void _init();
+void menu();
+void tanya(int*);
+void alokasi();
+void tambah();
+void tambah_pertama();
+void tambah_depan();
+void tambah_tengah();
+void tambah_belakang();
+void cari();
+
+//Main
+int main() {
+	bool lanjut = true;
+	int pilihan = 0;
+	do {
+		std::cout << "\n\n";
+		menu();
+		tanya(&pilihan);
+		switch (pilihan) {
+		case 1:
+			alokasi();
+			if (akhir == NULL) {
+				tambah_pertama();
+			}
+			else {
+				tambah();
+			}
+			break;
+		case 2:
+			cari();
+			break;
+		case 3:
+			tampil();
+			break;
+		default:
+			std::cout << "(Tekan y untuk keluar!)";
+			if (_getche() == 'y') {
+				lanjut = false;
+			}
+			break;
+		}
+	} while (lanjut);
+}
+void _init() {
+	awal = NULL;
+	akhir = NULL;
+}
+
+void menu() {
+	std::cout << "MENU\n";
+	std::cout << "1. Tambah data\n";
+	std::cout << "2. Cari data\n";
+	std::cout << "3. Tampil semua data\n";
+	std::cout << "?? Keluar\n";
+}
+
+void tanya(int* storage) {
+	std::cout << "Pilih menu : ";
+	std::cin >> *storage;
+}
+
+void alokasi() {
+	int data_baru;
+	baru = (TNode*)malloc(sizeof(struct TNode));
+	std::cout << "Masukan data : ";
+	std::cin >> data_baru;
+	baru->data = data_baru;
+}
+
+void tambah() {
+	int ps;
+	std::cout << "Pilihan penambahan : \n";
+	std::cout << "1.Tambah depan\n";
+	std::cout << "2.Tambah tengah\n";
+	std::cout << "3.Tambah belakang\n";
+	std::cout << "Pilih posisi : ";
+	std::cin >> ps;
+	switch (ps) {
+	case 1:tambah_depan(); break;
+	case 2:tambah_tengah(); break;
+	case 3:tambah_belakang(); break;
+	}
+}
+void tambah_pertama() {
+	awal = baru;
+	akhir = baru;
+	baru->next = NULL;
+	akhir->next = NULL;
+}
+void tambah_depan() {
+	baru->next = awal;
+	awal = baru;
+}
+void tambah_tengah() {
+
+}
+void tambah_belakang() {
+	baru->next = NULL;
+	akhir->next = baru;
+	akhir = baru;
+}
+
+void cari() {
+	int c_data = 0;
+	//std::vector<int> result;
+	bool ketemu = false;
+	std::cout << "Masukan data yang ingin dicari : ";
+	std::cin >> c_data;
+	if (awal == NULL) {
+		std::cout << "Nak cari apa ? LinkedList pun masi kosong!";
+	}
+	else {
+		bantu_cari = awal;
+		do {
+			if (bantu_cari->data == c_data) {
+				ketemu = true;
+				std::cout << "Data ditemukan";
+				std::cout << "Lokasi : " << bantu_cari;
+			}
+			else {
+				bantu_cari = bantu_cari->next;
+			}
+		} while (!ketemu && bantu_cari != NULL);
+		if (!ketemu) {
+			std::cout << "Data yang dicari tidak ditemukan!";
+		}
+	}
+}
+
+void tampil() {
+	if (awal == NULL) {
+		std::cout << "Nak lihat apa ? LinkedList pun masi kosong!";
+	}
+	else {
+		bantu_tampil = awal;
+		do {
+			std::cout << "Data : " << bantu_tampil->data;
+			bantu_tampil = bantu_tampil->next;
+		} while (bantu_tampil != NULL);
+	}
+}
